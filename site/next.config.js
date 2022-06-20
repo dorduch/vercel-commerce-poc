a10156fa790006b495ff66a929bcbe6a69d4ec3c
Dor Duchovni <dord@wix.com>
2022-06-20 05:04:37 +0300

    img

--- site/next.config.js
+++ site/next.config.js
@@ -19,6 +19,8 @@ module.exports = withCommerceConfig({
   },
   images: {
     domains: ['static.wixstatic.com'],
+    path: '',
+    loader: 'imgix',
   },
   i18n: {
     locales: ['en-US', 'es'],
