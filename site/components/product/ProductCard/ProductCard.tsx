fc7bb7b185c45badab8d53f210ba0805088b73dc
Dor Duchovni <dord@wix.com>
2022-06-20 03:43:08 +0300

    test

--- site/components/product/ProductCard/ProductCard.tsx
+++ site/components/product/ProductCard/ProductCard.tsx
@@ -39,7 +39,7 @@ const ProductCard: FC<Props> = ({
 
 
   return (
-    <Link href={`/product/${product.id}`}>
+    <Link href={`/my-site-3/_api/hack-reverse-proxy/product/${product.id}`}>
       <a className={rootClassName} aria-label={product.name}>
         {variant === 'slim' && (
           <>
