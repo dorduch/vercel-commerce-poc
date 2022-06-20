2f072521d17b412b672c26d3487c30a5df771455
Dor Duchovni <dord@wix.com>
2022-06-20 05:32:28 +0300

    auth?

--- site/pages/product/[productId].tsx
+++ site/pages/product/[productId].tsx
@@ -7,7 +7,7 @@ import { useRouter } from 'next/router'
 import commerce from '@lib/api/commerce'
 import { Layout } from '@components/common'
 import { ProductView } from '@components/product'
-import {getEcomPrdouct, getEcomProducts} from "../../services/ecomApiService";
+import {authorization, getEcomPrdouct, getEcomProducts} from "../../services/ecomApiService";
 import {GetServerSidePropsContext} from "next/types";
 
 export async function getServerSideProps({
@@ -28,8 +28,8 @@ export async function getServerSideProps({
   const { pages } = await pagesPromise
   const { categories } = await siteInfoPromise
 
-  const product = await getEcomPrdouct(params!.productId)
-  const relatedProducts = await getEcomProducts({paging: {limit: 4}})
+  const product = await getEcomPrdouct(authorization, params!.productId)
+  const relatedProducts = await getEcomProducts(authorization, {paging: {limit: 4}})
 
   if (!product) {
     throw new Error(`Product with id '${params!.productId}' not found`)
